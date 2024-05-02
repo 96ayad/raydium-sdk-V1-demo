d8d0fe1d8ec4c7330abe54c2fe4c64fe3b350df0
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-02 14:15:43 +0300

    config.ts

--- config.ts
+++ config.ts
@@ -2,7 +2,11 @@ import {
   ENDPOINT as _ENDPOINT,
   Currency,
   LOOKUP_TABLE_CACHE,
-  MAINNET_PROGRAM_ID,
+
+  /////////////////ayad/////////////
+  // MAINNET_PROGRAM_ID,
+  DEVNET_PROGRAM_ID,
+
   RAYDIUM_MAINNET,
   Token,
   TOKEN_PROGRAM_ID,
@@ -17,11 +21,16 @@ import {
 export const rpcUrl: string = 'https://xxx.xxx.xxx/'
 export const rpcToken: string | undefined = undefined
 
-export const wallet = Keypair.fromSecretKey(Buffer.from('<YOUR_WALLET_SECRET_KEY>'))
+///////////////////////ayad///////////////////////
+// export const wallet = Keypair.fromSecretKey(Buffer.from('<YOUR_WALLET_SECRET_KEY>'))
+// export const connection = new Connection('<YOUR_RPC_URL>');
+// export const PROGRAMIDS = MAINNET_PROGRAM_ID;
 
-export const connection = new Connection('<YOUR_RPC_URL>');
+export const wallet = Keypair.fromSecretKey(Buffer.from('<43EeRipwq7QZurfASn7CnYuJ14pVaCEv7KWav9vknt1bFR6qspYXC2DbaC2gGydrVx4TFtWfyCFkEaLLLMB2bZoT>'))
 
-export const PROGRAMIDS = MAINNET_PROGRAM_ID;
+export const connection = new Connection('<https://api.devnet.solana.com/>');
+
+export const PROGRAMIDS = DEVNET_PROGRAM_ID;
 
 export const ENDPOINT = _ENDPOINT;
 
