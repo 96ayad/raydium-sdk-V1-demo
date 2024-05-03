f0e3af87b8e01b8e7568d84c253fea4d0e233f2d
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-03 09:38:29 +0300

    import express, { Express, Request, Response } from "express"; import dotenv from "dotenv";
    dotenv.config();
    
    const app: Express = express();
    const port = process.env.PORT || 3000;
    
    app.get("/", (req: Request, res: Response) => {
      res.send("Express + TypeScript Server");
    });
    
    app.listen(port, () => {
      console.log(`[server]: Server is running at http://localhost:${port}`);
    });

--- src/utilsCreateMarket.ts
+++ src/utilsCreateMarket.ts
@@ -14,25 +14,21 @@ import {
 import { buildAndSendTx } from './util';
 
 ///////////////ayad////////////////
-//const express = require('express')
-// const app = express()
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-import {get, listen} from 'express';
+dotenv.config();
 
-const app = {
-  get: get,
-  listen: listen
-}
-//const port = process.env.PORT || 4000;
-const port = 4000;
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
 
 
 type TestTxInputInfo = {
