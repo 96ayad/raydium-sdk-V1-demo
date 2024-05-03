523d0febfb4a6d263fafc6e8e0387415525cf5ce
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-03 08:37:56 +0300

    export

--- config.ts
+++ config.ts
@@ -35,7 +35,7 @@ export const rpcToken: string | undefined = undefined
 const DEMO_FROM_SECRET_KEY = new Uint8Array(
   [151,250,133,160,178,197,133,103,69,122,236,210,204,163,134,138,41,3,125,57,8,168,214,17,218,120,180,227,245,234,75,72,10,76,127,170,65,248,245,58,114,27,168,242,66,37,79,216,141,207,121,134,27,72,177,85,105,137,186,168,39,146,175,38]
 );
-const wallet = Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
+export const wallet = Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
 
 
 export const connection = new Connection('https://api.devnet.solana.com/');
