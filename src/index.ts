98f42abf02df40f9d4fb5c3571efcd379bffa144
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-03 12:00:15 +0300

    index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,90 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+import {
+    MarketV2,
+    Token,
+  } from '@raydium-io/raydium-sdk';
+  import { Keypair } from '@solana/web3.js';
+  
+  import {
+    connection,
+    DEFAULT_TOKEN,
+    makeTxVersion,
+    PROGRAMIDS,
+    wallet,
+  } from '../config';
+  import { buildAndSendTx } from './util';
+  
+  // ///////////////ayad////////////////
+  // import express, { Express, Request, Response } from "express";
+  // import dotenv from "dotenv";
+  
+  // dotenv.config();
+  
+  // const app: Express = express();
+  // const port = process.env.PORT || 3000;
+  
+  // app.get("/", (req: Request, res: Response) => {
+  //   res.send("Express + TypeScript Server");
+  // });
+  
+  // app.listen(port, () => {
+  //   console.log(`[server]: Server is running at http://localhost:${port}`);
+  // });
+  
+  import express, { Express, Request, Response } from "express";
+  import dotenv from "dotenv";
+  
+  dotenv.config();
+  
+  const app: Express = express();
+  const port = process.env.PORT || 3000;
+  
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
+  
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+  
+  
+  
+  
+  type TestTxInputInfo = {
+    baseToken: Token
+    quoteToken: Token
+    wallet: Keypair
+  }
+  
+  export async function createMarket(input: TestTxInputInfo) {
+    // -------- step 1: make instructions --------
+    const createMarketInstruments = await MarketV2.makeCreateMarketInstructionSimple({
+      connection,
+      wallet: input.wallet.publicKey,
+      baseInfo: input.baseToken,
+      quoteInfo: input.quoteToken,
+      lotSize: 1, // default 1
+      tickSize: 0.01, // default 0.01
+      dexProgramId: PROGRAMIDS.OPENBOOK_MARKET,
+      makeTxVersion,
+    })
+  
+    return { txids: await buildAndSendTx(createMarketInstruments.innerTransactions) }
+  }
+  
+  async function howToUse() {
+    // const baseToken = DEFAULT_TOKEN.RAY // RAY
+    // const quoteToken = DEFAULT_TOKEN.USDC // USDC
+  
+    const baseToken = DEFAULT_TOKEN.sold // sold
+    const quoteToken = DEFAULT_TOKEN.WSOL // WSOL
+  
+    createMarket({
+      baseToken,
+      quoteToken,
+      wallet: wallet,
+    }).then(({ txids }) => {
+      /** continue with txids */
+      console.log('txids', txids)
+    })
+  }
+  
--- src/utilsCreateMarket.ts
+++ src/utilsCreateMarket.ts
@@ -30,21 +30,21 @@ import { buildAndSendTx } from './util';
 //   console.log(`[server]: Server is running at http://localhost:${port}`);
 // });
 
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+// import express, { Express, Request, Response } from "express";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+// const app: Express = express();
+// const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Express + TypeScript Server");
+// });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
 
 
 
